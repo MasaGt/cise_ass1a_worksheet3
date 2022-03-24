ae7079589c324c6810e6dff9a90695954adc18c2
MasaGt <MasaGt@users.noreply.github.com>
2022-03-24 16:08:04 +1300

    fixed a parameter for console.log

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-app.get('/', (req, res) => {
-    res.send("Hello World!");
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 const port = process.env.PORT || 8082;
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`));
+  console.log(`Server running on port ${port}`);
 });
