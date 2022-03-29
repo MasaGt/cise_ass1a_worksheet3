af3467d5533724ebf0f744762aa67b06b4011bce
MasaGt <MasaGt@users.noreply.github.com>
2022-03-29 14:53:05 +1300

    fixed api url

--- my-app/src/components/UpdateBookInfo.js
+++ my-app/src/components/UpdateBookInfo.js
@@ -19,7 +19,7 @@ class UpdateBookInfo extends Component {
   componentDidMount() {
     // console.log("Print id: " + this.props.match.params.id);
     axios
-      .get("/api/books" + this.props.match.params.id)
+      .get("/api/books/" + this.props.match.params.id)
       .then((res) => {
         // this.setState({...this.state, book: res.data})
         this.setState({
